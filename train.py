88781423b729d604e38bd0faabde89ebbf6f37a1
WojciechMat <wmatsad@gmail.com>
2023-08-18 14:35:43 +0200

    comments and cleanup

--- train.py
+++ train.py
@@ -21,9 +21,18 @@ from data.dataloaders import load_vocab, load_tokenizers, create_dataloaders
 
 @hydra.main(config_path="config", config_name="conf", version_base=None)
 def main(cfg: DictConfig):
-    module, run_id = load_trained_model(cfg)
+    # load tokenizers and vocab
+    spacy_de, spacy_en = load_tokenizers()
+    vocab_src, vocab_tgt = load_vocab(spacy_de, spacy_en, cfg.data_slice)
+
+    # Train a model
+    model, run_id = train_model(vocab_src, vocab_tgt, spacy_de, spacy_en, cfg)
+
+    # save weights to a file
     file_path = f"models/{cfg.file_prefix}-{run_id}-final.pt"
-    torch.save(module.state_dict(), file_path)
+    torch.save(model.state_dict(), file_path)
+
+    # print a run_id of the model
     print(run_id)
 
 
@@ -40,12 +49,8 @@ def load_trained_model(cfg) -> tuple[nn.Module, str]:
     # load tokenizers and vocab
     spacy_de, spacy_en = load_tokenizers()
     vocab_src, vocab_tgt = load_vocab(spacy_de, spacy_en, cfg.data_slice)
-    # if model from cfg does not exist - train a new model
-    if not exists(cfg.model_path):
-        model, run_id = train_model(vocab_src, vocab_tgt, spacy_de, spacy_en, cfg)
-    else:
-        model = make_model(len(vocab_src), len(vocab_tgt), n=6)
-        model.load_state_dict(torch.load(cfg.model_path))
+    model, run_id = train_model(vocab_src, vocab_tgt, spacy_de, spacy_en, cfg)
+
     return model, run_id
 
 
@@ -68,11 +73,19 @@ def train_model(
     cfg: DictConfig,
 ) -> tuple[nn.Module, str]:
     run_id = initialize_wandb(cfg)
+
+    # Get the index for padding token
     pad_idx = vocab_tgt["<blank>"]
+
+    # define model parameters and create the model
     d_model = 512
     model = make_model(len(vocab_src), len(vocab_tgt), n=6)
     module = model
+
+    # Set LabelSmoothing as a criterion for loss calculation
     criterion = LabelSmoothing(size=len(vocab_tgt), padding_idx=pad_idx, smoothing=0.1)
+
+    # Create dataloaders
     train_dataloader, valid_dataloader = create_dataloaders(
         vocab_src,
         vocab_tgt,
@@ -82,16 +95,21 @@ def train_model(
         batch_size=cfg.batch_size,
         max_padding=cfg.max_padding,
     )
+
+    # Define optimizer and learning rate scheduler
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.base_lr, betas=(0.9, 0.98), eps=1e-9)
     lr_scheduler = LambdaLR(
         optimizer=optimizer,
         lr_lambda=lambda step: rate(step, d_model, factor=1, warmup=cfg.warmup),
     )
+
     train_state = TrainState()
 
     for epoch in range(cfg.num_epochs):
         model.train()
-        print(f"Epoch {epoch} Training ====", flush=True)
+        print(f"Epoch {epoch}", flush=True)
+
+        # Train model for one epoch
         t_loss, train_state = train_epoch(
             (Batch(b[0], b[1], pad_idx) for b in train_dataloader),
             model,
@@ -102,16 +120,19 @@ def train_model(
             train_state=train_state,
         )
 
+        # Save checkpoint after each epoch
         file_path = "models/%s-%s-%.2d.pt" % (cfg.file_prefix, run_id, epoch)
         torch.save(module.state_dict(), file_path)
 
-        print(f"Epoch {epoch} Validation ====", flush=True)
+        print(f"Epoch {epoch} Validation", flush=True)
         model.eval()
+        # Evaluate the model on validation set
         sloss, val_state = val_epoch(
             (Batch(b[0], b[1], pad_idx) for b in valid_dataloader),
             model,
             SimpleLossCompute(module.generator, criterion),
         )
+        # Log validation and training losses
         print(sloss)
         wandb.log({"val/loss": sloss, "train/loss": t_loss})
 
@@ -145,26 +166,36 @@ def train_epoch(
     total_loss = 0
     tokens = 0
     n_accum = 0
-    i = 0
+    i = 0  # iteration counter
+    # create progress bar
     pbar = tqdm(data_iter)
     for batch in pbar:
-
         out = model.forward(batch.src, batch.tgt, batch.src_mask, batch.tgt_mask)
+
         loss, loss_node = loss_compute(out, batch.tgt_y, batch.ntokens)
         loss_node.backward()
+
         train_state.step += 1
         train_state.samples += batch.src.shape[0]
         train_state.tokens += batch.ntokens
+
+        # Update the model parameters and optimizer gradients every `accum_iter` iterations
         if i % accum_iter == 0:
             optimizer.step()
             optimizer.zero_grad(set_to_none=True)
             n_accum += 1
             train_state.accum_step += 1
         i += 1
+
+        # Update learning rate scheduler
         scheduler.step()
+
+        # Update loss and token counts
         total_loss += loss
         total_tokens += batch.ntokens
         tokens += batch.ntokens
+
+        # Display progress every 2 iterations
         if i % 2 == 1:
             lr = optimizer.param_groups[0]["lr"]
             elapsed = time.time() - start
@@ -173,9 +204,12 @@ def train_epoch(
             pbar.set_description(
                 f"Epoch Step: {i:6d} | Accumulation Step: {n_accum:3d} | Loss: {loss:6.2f} | Tokens / Sec {tok_rate:7.1f} | Learning Rate: {lr:6.1e}"
             )
+
+            # log the loss each to Weights and Biases
             wandb.log({"train_steps/loss": loss})
         del loss
         del loss_node
+    # Return average loss over all tokens and updated train state
     return total_loss / total_tokens, train_state
 
 
@@ -190,20 +224,22 @@ def val_epoch(
     tokens = 0
     for i, batch in enumerate(data_iter):
         out = model.forward(batch.src, batch.tgt, batch.src_mask, batch.tgt_mask)
+
         loss, loss_node = loss_compute(out, batch.tgt_y, batch.ntokens)
+
         total_loss += loss
         total_tokens += batch.ntokens
         tokens += batch.ntokens
+
         del loss
         del loss_node
+    # Return average loss over all tokens and updated train state
     return total_loss / total_tokens, train_state
 
 
 def rate(step: int, model_size: int, factor: float, warmup: int) -> float:
-    """
-    we have to default the step to 1 for LambdaLR function
-    to avoid zero raising to negative power.
-    """
+    # we have to default the step to 1 for LambdaLR function
+    # to avoid zero raising to negative power.
     if step == 0:
         step = 1
     return factor * (model_size ** (-0.5) * min(step ** (-0.5), step * warmup ** (-1.5)))
